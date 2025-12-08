5da1ee34af9e9f6f6a6528f38b6ac79b4027ba1e
Joe Clark <jxclark@live.com>
2025-12-08 10:09:37 -0500

    05: theme-styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
-import { getQueryClient, trpc } from '@/trpc/server';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import { Client } from './client';
-import { Suspense } from 'react';
+import { requireAuth } from '@/lib/auth-utils';
 
 const Page = async () => {
-  const queryClient = getQueryClient();
-
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
-
+  await requireAuth();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+      Protected Server Component
     </div>
   );
 };
