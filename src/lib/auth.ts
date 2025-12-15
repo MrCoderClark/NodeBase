86cb81f8900447108e07e6858be580d3c2cf0f48
Joseph Clark <weirdlookingjay@live.com>
2025-12-15 14:01:51 -0500

    10: add-payment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,9 @@
+import { polar, checkout, portal } from '@polar-sh/better-auth';
+
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import prisma from './db';
+import { polarClient } from './polar';
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +13,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "6f93a9f3-4c28-4559-94fa-0d83c843e0ac",
+              slug: "pro",
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal()
+      ]
+    }) as any,
+  ],
 });
